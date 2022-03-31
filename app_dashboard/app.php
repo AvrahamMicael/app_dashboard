3d91627bf5b5bfd4b5e5cf763778535d975b7e66
AvrahamMicael <avrahammicael@protonmail.com>
2022-03-31 11:08:53 -0300

    ajax, dados do bd

--- app_dashboard/app.php
+++ app_dashboard/app.php
@@ -5,6 +5,12 @@
         public $data_fim;
         public $numero_vendas;
         public $total_vendas;
+        public $clientes_ativos;
+        public $clientes_inativos;
+        public $total_reclamacoes;
+        public $total_elogios;
+        public $total_sugestoes;
+        public $total_despesas;
 
         public function __get($atributo) {
             return $this->$atributo;
@@ -31,7 +37,7 @@
                     "$this->pass"
                 );
 
-                $conexao->exec('set charset utf8');//ou set charset set utf8?
+                $conexao->exec('set charset utf8');
 
                 return $conexao;
 
@@ -64,9 +70,10 @@
 
             return $stmt->fetch(PDO::FETCH_OBJ)->numero_vendas;
         }
+
         public function getTotalVendas() {
             $query = "
-                select count(total) as total_vendas
+                select sum(total) as total_vendas
                 from tb_vendas
                 where data_venda between :data_inicio and :data_fim
             ";
@@ -76,7 +83,47 @@
             $stmt->bindValue(':data_fim', $this->dashboard->__get('data_fim'));
             $stmt->execute();
 
-            return $stmt->fetch(PDO::FETCH_OBJ)->numero_vendas;
+            return $stmt->fetch(PDO::FETCH_OBJ)->total_vendas;
+        }
+
+        public function getClientes($status) {
+            $query = "
+                select count(*) as clientes
+                from tb_clientes
+                where cliente_ativo = '{$status}'
+            ";
+
+            $stmt = $this->conexao->query($query);
+
+            return $stmt->fetch(PDO::FETCH_OBJ)->clientes;
+        }
+
+        public function getTotalContatos($tipo) {
+            $query = "
+                select count(*) as contato
+                from tb_contatos
+                where tipo_contato = '{$tipo}'
+            ";
+            
+
+            $stmt = $this->conexao->query($query);
+
+            return $stmt->fetch(PDO::FETCH_OBJ)->contato;
+        }
+
+        public function getTotalDespesas() {
+            $query = "
+                select sum(total) as total
+                from tb_despesas
+                where data_despesa between :data_inicio and :data_fim
+            ";
+
+            $stmt = $this->conexao->prepare($query);
+            $stmt->bindValue(':data_inicio', $this->dashboard->__get('data_inicio'));
+            $stmt->bindValue(':data_fim', $this->dashboard->__get('data_fim'));
+            $stmt->execute();
+
+            return $stmt->fetch(PDO::FETCH_OBJ)->total;
         }
     }
 
@@ -85,12 +132,33 @@
     $dashboard = new Dashboard();
     $dashboard->__set('data_inicio', '2018-08-01');
     $dashboard->__set('data_fim', '2018-08-31');
+    
+    //competencia do select
+    $competencia = explode('-', $_GET['competencia']);
+    $ano = $competencia[0];
+    $mes = $competencia[1];
+
+    $dias_do_mes = cal_days_in_month(CAL_GREGORIAN, $mes, $ano);
+
+    $dashboard->__set('data_inicio', "$ano-$mes-01");
+    $dashboard->__set('data_fim', "$ano-$mes-$dias_do_mes");
+    //
 
     $conexao = new Conexao();
     
     $bd = new Bd($conexao, $dashboard);
+
     
     $dashboard->__set('numero_vendas', $bd->getNumeroVendas());
-    print_r($dashboard);
+    $dashboard->__set('total_vendas', $bd->getTotalVendas());
+    $dashboard->__set('clientes_ativos', $bd->getClientes(1));
+    $dashboard->__set('clientes_inativos', $bd->getClientes(0));
+    $dashboard->__set('total_reclamacoes', $bd->getTotalContatos(1));
+    $dashboard->__set('total_elogios', $bd->getTotalContatos(2));
+    $dashboard->__set('total_sugestoes', $bd->getTotalContatos(3));
+    $dashboard->__set('total_despesas', $bd->getTotalDespesas());
+
+
+    echo json_encode($dashboard);
 
 ?>
--- app_dashboard/script.js
+++ app_dashboard/script.js
@@ -1,31 +1,42 @@
 $(document).ready(() => {
-	
-    // $("#documentacao").on('click', () => {
-    //     $("#pagina").load('documentacao.html')
-    // })
-
-    // $("#suporte").on('click', () => {
-    //     $("#pagina").load('suporte.html')
-    // })
 
     $("#documentacao").on('click', () => {
-        // $.get('documentacao.html', data => {
-        //     $("#pagina").html(data)
-        // })
-        
-        $.post('documentacao.html', data => {
-            $("#pagina").html(data)
-        })
+        $("#pagina").load('documentacao.html')
     })
     
     $("#suporte").on('click', () => {
-        // $.get('suporte.html', data => {
-        //     $("#pagina").html(data)
-        // })
-        
-        $.post('suporte.html', data => {
-            $("#pagina").html(data)
+        $("#pagina").load('suporte.html')
+    })
+
+    //dashboard click
+    $("#dashboard").on('click', () => {
+        $("#pagina").load('pagina.html')
+    })
+
+    //ajax - select de competencia
+    $('select').on('change', e => {
+        if($(e.target).val() == '') return false
+
+        const competencia = $(e.target).val()
+
+        $.ajax({
+            type: 'get',
+            url: 'app.php',
+            data: `competencia=${competencia}`,
+            dataType: 'json',
+            success: dados => {
+                $("#numero_vendas").html(dados.numero_vendas)
+                $("#total_vendas").html(dados.total_vendas)
+                $("#clientes_ativos").html(dados.clientes_ativos)
+                $("#clientes_inativos").html(dados.clientes_inativos)
+                $("#total_reclamacoes").html(dados.total_reclamacoes)
+                $("#total_elogios").html(dados.total_elogios)
+                $("#total_sugestoes").html(dados.total_sugestoes)
+                $("#total_despesas").html(dados.total_despesas)
+            },
+            error: erro => console.log(erro.statusText)
         })
+
     })
 
 })
